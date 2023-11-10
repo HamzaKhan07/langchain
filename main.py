c3a310f7a3caff3af4a75abf2381fe1001c338f0
hamza <hamzakhan48208@gmail.com>
2023-11-10 15:41:00 +0530

    feat:initial

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import HuggingFaceHub
 import streamlit as st
 import gc
 
 st.header("Talk with PDF")
 
 
-def load_result():
+def load_result(query):
+    from langchain.embeddings import HuggingFaceEmbeddings
+    from langchain.vectorstores import FAISS
+    from langchain.chains.question_answering import load_qa_chain
+    from langchain.llms import HuggingFaceHub
+
     # load garbage collector
     gc.enable()
+
+    # move imports inside the function
+
     embeddings = HuggingFaceEmbeddings()
     vectordb = FAISS.load_local('faiss_index', embeddings)
 
@@ -18,7 +22,7 @@ def load_result():
     llm = HuggingFaceHub(repo_id="google/flan-ul2", huggingfacehub_api_token="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")
     chain = load_qa_chain(llm=llm, chain_type="stuff")
 
-    similar_chunks = vectordb.similarity_search(query=query, k=2)
+    similar_chunks = vectordb.similarity_search(query=query, k=2)[:2]  # Fetch only the necessary data
     response = chain.run(input_documents=similar_chunks, question=query)
 
     st.write(response)
@@ -27,6 +31,8 @@ def load_result():
     st.write(similar_chunks[1])
 
     # delete all the unused vars
+    del embeddings
+    del vectordb
     del llm
     del chain
     del similar_chunks
@@ -37,8 +43,6 @@ def load_result():
 
 
 if __name__ == '__main__':
-
     query = st.text_input("Enter your question")
     if query:
-        load_result()
-
+        load_result(query)

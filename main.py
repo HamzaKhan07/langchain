13a218e2fb469297d1dcf0b6ea29db817cfc794d
hamza <hamzakhan48208@gmail.com>
2023-11-10 16:00:14 +0530

    feat:initial

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ st.header("Talk with PDF")
 def load_result(query):
     from langchain.embeddings import HuggingFaceEmbeddings
     from langchain.vectorstores import FAISS
-    from langchain.chains.question_answering import load_qa_chain
+    from langchain.chains import RetrievalQA
     from langchain.llms import HuggingFaceHub
 
     # load garbage collector
@@ -17,25 +17,23 @@ def load_result(query):
 
     embeddings = HuggingFaceEmbeddings()
     vectordb = FAISS.load_local('faiss_index', embeddings)
+    retriever = vectordb.as_retriever(score_threshold=0.7)
 
     # question answer chain
     llm = HuggingFaceHub(repo_id="google/flan-ul2", huggingfacehub_api_token="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")
-    chain = load_qa_chain(llm=llm, chain_type="stuff")
+    chain = RetrievalQA.from_chain_type(llm=llm, chain_type='stuff', retriever=retriever)
 
-    similar_chunks = vectordb.similarity_search(query=query, k=2)[:2]  # Fetch only the necessary data
-    response = chain.run(input_documents=similar_chunks, question=query)
+    response = chain(query)
 
     st.write(response)
-    # references
-    st.write(similar_chunks[0])
-    st.write(similar_chunks[1])
+
+    # put references here
 
     # delete all the unused vars
     del embeddings
     del vectordb
     del llm
     del chain
-    del similar_chunks
     del response
 
     # release garbage collector
